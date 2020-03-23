5184348fe24eda70ae2e73d52d10a21c82bfd36f
Tenyo <THEG4ME@abv.bg>
2020-03-23 14:01:25 +0200

    Adding light java script for Title change when onmouseover hero icons

--- Web/DotaESport.Web/wwwroot/js/site.js
+++ Web/DotaESport.Web/wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+function takeName(nameHero) {
+    document.getElementById("heroName").innerHTML = nameHero;
+}
