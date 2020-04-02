12c1f942e1a0888011f3495787550d0aa3aec33b
Tenyo <THEG4ME@abv.bg>
2020-04-02 19:50:21 +0300

    Working project checkpoint

---
+++ Web/DotaESport.Web.ViewModels/Articles/ArticleCommentViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DotaESport.Data.Models;
+using DotaESport.Services.Mapping;
+using Ganss.XSS;
+
+namespace DotaESport.Web.ViewModels.Articles
+{
+    public class ArticleCommentViewModel : IMapFrom<Comment>
+    {
+        public int Id { get; set; }
+
+        public int? ParentId { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public string Content { get; set; }
+
+        public string SanitizedContent => new HtmlSanitizer().Sanitize(this.Content);
+
+        public string UserUserName { get; set; }
+    }
+}
--- Web/DotaESport.Web.ViewModels/Articles/ArticleViewModel.cs
+++ Web/DotaESport.Web.ViewModels/Articles/ArticleViewModel.cs
@@ -1,12 +1,15 @@
-﻿namespace DotaESport.Web.ViewModels.Articles
+﻿using Ganss.XSS;
+
+namespace DotaESport.Web.ViewModels.Articles
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Text;
 
+    using AutoMapper;
     using DotaESport.Data.Models;
     using DotaESport.Services.Mapping;
-
 
     public class ArticleViewModel : IMapFrom<Article>, IMapTo<Article>
     {
@@ -22,8 +25,12 @@
 
         public string Content { get; set; }
 
+        public string SanitizedContent => new HtmlSanitizer().Sanitize(this.Content);
+
         public string UserUserName { get; set; }
 
         public int VotesCount { get; set; }
+
+        public IEnumerable<ArticleCommentViewModel> Comments { get; set; }
     }
 }
