73c23f16809443fdff092f295b7d371514da4a47
Tenyo <THEG4ME@abv.bg>
2020-04-03 19:02:30 +0300

    Add IVotesServices.cs VotesService.cs

---
+++ Services/DotaESport.Services.Data/IVotesServices.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotaESport.Services.Data
+{
+    public interface IVotesServices
+    {
+        Task VoteAsync(int postId, string userId, bool IsUpVote);
+    }
+}
---
+++ Services/DotaESport.Services.Data/VotesService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotaESport.Services.Data
+{
+    public class VotesService : IVotesServices
+    {
+        public Task VoteAsync(int postId, string userId, bool IsUpVote)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
