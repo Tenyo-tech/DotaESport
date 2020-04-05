73f28be4ef3d66f84f97de7733f9452d9d831816
Tenyo <THEG4ME@abv.bg>
2020-04-05 17:51:50 +0300

    Working on _Layout.cshtml

---
+++ Tests/DotaESport.Services.Data.Tests/HomePageTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using DotaESport.Web;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace DotaESport.Services.Data.Tests
+{
+    public class HomePageTests
+    {
+        [Fact]
+        public async Task HomePageShouldHaveTitle()
+        {
+            var serverFactory = new WebApplicationFactory<Startup>();
+            var client = serverFactory.CreateClient();
+
+            var response = await client.GetAsync("/");
+            var responseAsString = await response.Content.ReadAsStringAsync();
+
+            Assert.Contains("<h3", responseAsString);
+        }
+    }
+}
