ed06967914c0a2df7deae5b3f662e8a22b9b94b1
Tenyo <THEG4ME@abv.bg>
2020-04-15 17:05:16 +0300

    Working Project CheckPoint

---
+++ Data/DotaESport.Data/Seeding/AddRoleToUserSeeder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotaESport.Common;
+using DotaESport.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DotaESport.Data.Seeding
+{
+    public class AddRoleToUserSeeder : ISeeder
+    {
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<ApplicationRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+            var role = await roleManager.FindByNameAsync(GlobalConstants.AdminRoleName);
+
+            var user = await userManager.FindByNameAsync(GlobalConstants.TenyoUserName);
+            var exist = dbContext.UserRoles.Any(x => x.UserId == user.Id &&
+                                                     x.RoleId == role.Id);
+
+            if (exist)
+            {
+                return;
+            }
+            await dbContext.UserRoles.AddAsync(new IdentityUserRole<string>
+            {
+                RoleId = role.Id,
+                UserId = user.Id,
+            });
+
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ Web/DotaESport.Web/Controllers/TestController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotaESport.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public TestController(
+            UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.roleManager = roleManager;
+        }
+
+        public async Task<IActionResult> Test()
+        {
+            var result = await this.roleManager.CreateAsync(new IdentityRole
+            {
+                Name = "Admin",
+            });
+            var user = await this.userManager.GetUserAsync(this.User);
+            var user2 = await this.userManager.FindByNameAsync("Tenyo@abv.bg");
+            await this.userManager.AddToRoleAsync(user2, "Admin");
+
+            return this.Json(result);
+        }
+    }
+}
