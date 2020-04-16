f186f10e3843d32ba7e81a6935c159fa145a8d01
Tenyo <THEG4ME@abv.bg>
2020-04-16 17:54:29 +0300

    Add ApplicationDbContextInMemoryFactory.cs

---
+++ Tests/DotaESport.Services.Data.Tests/ApplicationDbContextInMemoryFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DotaESport.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotaESport.Services.Data.Tests
+{
+    public class ApplicationDbContextInMemoryFactory
+    {
+        public static ApplicationDbContext InitializeContext()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            return new ApplicationDbContext(options);
+        }
+    }
+}
